850edbdb705c7bf4501fe2430f07dc070ed44bff
Rebekah Garris <rebekahlgarris@gmail.com>
2023-09-13 15:54:07 -0500

    done with task 6

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,4 +1,34 @@
 package org.launchcode.techjobs.oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        this.id = nextId;
+        nextId++;
+    }
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
